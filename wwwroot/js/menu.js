1e0be800aeb075dce8f97be74defaa43dc4b9a23
Martynas Deksnys <martynas@deksnys.eu>
2025-09-16 17:52:52 +0300

    Add abhorrent JavaScript

--- wwwroot/js/menu.js
+++ wwwroot/js/menu.js
@@ -1,24 +1,41 @@
+import startGame from "./main.js";
+
 var connection = new signalR.HubConnectionBuilder().withUrl("/menuHub").build();
 
 var joinButton = document.getElementById("join-btn");
+var startButton = document.getElementById("start-btn");
 joinButton.disabled = true;
+startButton.disabled = true;
 
 connection.start().then(function () {
     joinButton.disabled = false;
+    startButton.disabled = false;
 }).catch(function (err) {
     return console.error(err.toString());
 });
 
-connection.on("JoinGame", function (matchId) {
-    console.log("Joined game: " + matchId);
-    // Actually do something here
-    // Hide elements etc.
-});
-
 joinButton.addEventListener("click", function (e) {
     var matchId = document.getElementById("matchId").value;
-    connection.invoke("JoinGame", matchId).catch(function (err) {
+    connection.invoke("JoinMatch", matchId).catch(function (err) {
         return console.error(err.toString());
     });
     event.preventDefault();
+});
+
+startButton.addEventListener("click", function (e) {
+    connection.invoke("NewMatch").catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+connection.on("JoinMatch", function (matchId) {
+    console.log("Joined game: " + matchId);
+    document.getElementById("matchIdText").textContent = matchId;
+
+    startGame()
+});
+
+connection.on("JoinFailed", function () {
+    console.log("Failed to join game");
 });
