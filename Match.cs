5624f8f6a68bdf9585e4dacd804aaea5486ce5e7
Martynas Deksnys <martynas@deksnys.eu>
2025-09-16 17:43:12 +0300

    Add IEquatable for Match class

--- Match.cs
+++ Match.cs
@@ -1,7 +1,9 @@
-public class Match
+public class Match : IEquatable<Match>
 {
     public string id { get; set; }
     public List<Player> players = new List<Player>();
 
     public Match(string matchId) => this.id = matchId;
+
+    public bool Equals(Match? other) => other is not null && this.id == other.id;
 }
