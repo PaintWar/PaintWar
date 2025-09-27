71921cf8122a3c77cdd43bac7bb3ed6580fafb81
petrasmac <maciul.petras@gmail.com>
2025-09-27 12:42:33 +0300

    Added LifePaint counter

--- Assets/Scripts/Multiplayer/Player.cs
+++ Assets/Scripts/Multiplayer/Player.cs
@@ -2,9 +2,16 @@ public class Player
 {
     public string Id { get; set; }
     public int Number { get; set; }
+    public LifePaint LifePaint { get; }
+
     public Player(string id, int number)
     {
         this.Id = id;
         this.Number = number;
+        this.LifePaint = new LifePaint();
     }
+
+    public void RewardLifePaint(int amount) => LifePaint.Add(amount);
+
+    public bool TrySpendLifePaint(int cost) => LifePaint.Spend(cost);
 }
