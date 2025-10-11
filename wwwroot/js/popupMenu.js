1733fc434d1efe18ffcce9b791c3aa5eddd97654
Martynas Deksnys <martynas@deksnys.eu>
2025-10-11 10:24:14 +0300

    Replace external 'X' SVG with a simpler one

--- wwwroot/js/popupMenu.js
+++ wwwroot/js/popupMenu.js
@@ -112,8 +112,7 @@ function colorContent(connection, player) {
             span.innerHTML = '<svg viewbox="0 0 100 100"><circle r=38 cx=50 cy=50 fill=none stroke="#111" stroke-width=10 /></svg>';
         }
         else if (associatedPlayers[i] !== null) {
-            // https://www.svgrepo.com/svg/12848/x-symbol, Public Domain (CC0)
-            span.innerHTML = '<svg fill="#111" height="100%" width="100%" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-46.08 -46.08 552.93 552.93" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0" transform="translate(0,0), scale(1)"><rect x="-46.08" y="-46.08" width="552.93" height="552.93" rx="0" fill="#7ed0ec" strokewidth="0"></rect></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="4.60775"></g><g id="SVGRepo_iconCarrier"> <path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55 c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55 c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505 c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55 l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719 c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"></path> </g></svg>';
+            span.innerHTML = '<svg viewbox="0 0 100 100"><line x1="10" y1="10" x2="90" y2="90" stroke="black" stroke-width="10" /><line x1="10" y1="90" x2="90" y2="10" stroke="black" stroke-width="10" /></svg>';
         }
 
         clickInvoke(connection, span, ["ChangeColor", player.privateId, i]);
